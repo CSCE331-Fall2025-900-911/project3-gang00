774657259515f0fe8a63acf62d84b884d0ba0f21
haolin0211 <haolin0211@tamu.edu>
2025-11-12 08:35:41 -0600

    Delete public/textToSpeech.js

--- public/textToSpeech.js
+++
@@ -1,4 +0,0 @@
-// Placeholder, this file is to implement the text-to-speech function
-// This function will use the embedded function of the browser(because this is free), google API for this function will use money
-// Should have start and stop function for this tts
-// Should skip the field that not need to be speaked(maybe nothing need to skip)
