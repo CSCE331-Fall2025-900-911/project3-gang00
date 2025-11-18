53aba7645d64959f5076bf75f82457b598ff5b80
Hao Lin <haolin0211@tamu.edu>
2025-11-18 14:22:54 -0600

    update manager page

--- public/manager.js
+++ public/manager.js
@@ -1,3 +1,12 @@
 function goToInventory() {
     window.location.href = "/manager/inventory";
 }
+
+function goToXreport(){
+    window.location.href = "/manager/xreport";
+
+}
+
+function goToZreport(){
+    window.location.href = "/manager/zreport";
+}
